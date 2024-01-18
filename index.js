4014642bf0caae3bb2ad3019f86fd14b65bd384b
Cyril Arrachequesne <cyrilou564@gmail.com>
2024-01-18 15:01:30 +0100

    🔨 add whoami route

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ app.get("/", (req, res) => {
 	res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.get("/api/whoami", function (req, res) {
+	const ipaddress = req.headers["x-forwarded-for"].split(",")[0];
+	const language = req.headers["accept-language"];
+	const software = req.headers["user-agent"];
+	res.json({ ipaddress, language, software });
+});
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 3000, () => {
 	console.log("Your app is listening on port " + listener.address().port);
